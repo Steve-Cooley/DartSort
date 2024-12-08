3ccadeb4a25dc1e8b9e958957bed493018d7f7bc
steve-cooley <scooley138@gmail.com>
2024-12-07 16:57:06 -0800

    Refactor: rename variable in insertion.

--- lib/sorters/insertion.dart
+++ lib/sorters/insertion.dart
@@ -1,15 +1,15 @@
 List<int> insertionSort(List<int> inp) {
   var outp = List<int>.from(inp);
   for (int i = 1; i < inp.length; ++i) {
-    int key = outp[i];
+    int tmp = outp[i];
     int j = i - 1;
 
-    while (j >= 0 && outp[j] > key) {
+    while (j >= 0 && outp[j] > tmp) {
       // right shift
       outp[j + 1] = outp[j];
       --j;
     }
-    outp[j + 1] = key;
+    outp[j + 1] = tmp;
   }
   return outp;
 }
