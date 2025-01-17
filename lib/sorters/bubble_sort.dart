23bc1d569a60172d28644280142f393e7792acf8
steve-cooley <scooley138@gmail.com>
2025-01-16 22:02:21 -0800

    "Write mutating bubbleSort and test it."

--- lib/sorters/bubble_sort.dart
+++ lib/sorters/bubble_sort.dart
@@ -1,12 +1,10 @@
 List<int> bubbleSort(List<int> inp) {
   /// Sort with bubbles.
-  print("Entering bubblesort");
   // make a copy of input list cuz immutability.
   var outp = List<int>.from(inp);
 
   for (int i = 0; i < outp.length; ++i) {
     for (int j = 0; j < outp.length - i - 1; ++j) {
-      // print("taco");
       // swap elements if L > R
       if (outp[j] > outp[j + 1]) {
         int temp = outp[j];
@@ -16,4 +14,18 @@ List<int> bubbleSort(List<int> inp) {
     }
   }
   return outp;
+}
+
+void bubbleSortMutating(List<int> inp) {
+  // Mutate input and sort with bubbles.
+  for (int i = 0; i < inp.length; ++i) {
+    for (int j = 0; j < inp.length - i - 1; ++j) {
+      // swap elements if L > R
+      if (inp[j] > inp[j + 1]) {
+        int temp = inp[j];
+        inp[j] = inp[j + 1];
+        inp[j + 1] = temp;
+      }
+    }
+  }
 }
