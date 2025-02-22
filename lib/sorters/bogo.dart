0ce39290497b9daf865450a6626658c53db15a13
steve-cooley <scooley138@gmail.com>
2025-02-21 16:54:48 -0800

    Delete "fixme" about making bogosort immutable. Dropping that. Some functions will have immutable versions, but not all, and definitely not bogosort.  LOL.

--- lib/sorters/bogo.dart
+++ lib/sorters/bogo.dart
@@ -5,8 +5,6 @@ import 'package:sorting_algorithms/utils.dart';
 List<int> bogoSort(List<int> inp) {
   /// Randomly shuffle elements until sorted.
   /// Complexity O(n!)
-  /// FIXME: Make this more immutable. Shouldn't return the same list that
-  /// it takes. Trivial, but I want it to have it's own git commit.
   while (!isSorted(inp)) {
     inp.shuffle();
   }
