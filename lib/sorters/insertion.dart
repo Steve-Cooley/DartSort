36cc107f0df30d2009e07697d8f0e3a3e8ede233
steve-cooley <scooley138@gmail.com>
2025-04-11 16:44:09 -0700

    Create and test mutatingInsertionSort. Reorganize.

--- lib/sorters/insertion.dart
+++ lib/sorters/insertion.dart
@@ -1,9 +1,8 @@
-List<int> insertionSort(List<int> inp) {
+List<int> insertionSortFunctional(List<int> inp) {
   final outp = List<int>.from(inp);
   for (int i = 1; i < inp.length; ++i) {
     int tmp = outp[i];
     int j = i - 1;
-
     while (j >= 0 && outp[j] > tmp) {
       // right shift
       outp[j + 1] = outp[j];
@@ -12,4 +11,17 @@ List<int> insertionSort(List<int> inp) {
     outp[j + 1] = tmp;
   }
   return outp;
+}
+
+List<int> insertionSortMutating(List<int> inp) {
+  for (int i = 1; i < inp.length; ++i) {
+    int tmp = inp[i];
+    int j = i - 1;
+    while (j >= 0 && inp[j] > tmp) {
+      inp[j + 1] = inp[j];
+      --j;
+    }
+    inp[j + 1] = tmp;
+  }
+  return inp;
 }
--- test/sorting_algos_test.dart
+++ test/sorting_algos_test.dart
@@ -8,16 +8,39 @@ import 'package:collection/collection.dart' as coll; // for listEquality
 import 'dart:math';
 
 void main() {
-  final sortedList0 = generateOrderedList(10);
+  final sortedList0 = generateOrderedList(100);
   final List<int> scrambledList0 = List.from(sortedList0)..shuffle();
 
   testBubbleSortGroup(scrambledList0, sortedList0);
   testShellSort(scrambledList0, sortedList0);
+  testInsertionSortMutating(scrambledList0, sortedList0);
+  testInsertionsSortFunctional(scrambledList0, sortedList0);
 }
+
+void testInsertionSortMutating(List<int> unsorted, List<int> sorted) {
+  //
+  test("Test insertion sort (mutating).", () {
+    // Copy scrambled list
+    List<int> testList0 = List.from(unsorted);
+    expect(unsorted, isNot(sorted), reason: "Avoid false successes.");
+    insertionSortMutating(testList0);
+    expect(testList0, equals(sorted));
+  });
+}
+
+void testInsertionsSortFunctional(List<int> unsorted, List<int> sorted) {
+  test('Test insertion sort non-mutating', () {
+    List<int> testList0 = List.from(unsorted);
+    expect(unsorted, isNot(sorted), reason: "Avoid false successes");
+    List<int> result = insertionSortMutating(testList0);
+    expect(result, equals(sorted));
+  });
+}
+
+void testBogoSort() {}
 
 void testShellSort(List<int> unsorted, List<int> sorted) {
   group('Testing Shellsort and variations.', () {
-    print('Testing shellsort variations');
     test('Test mutating shellsort.', () {
       // copy scrambled list
       List<int> testList0 = List.from(unsorted);
@@ -26,20 +49,15 @@ void testShellSort(List<int> unsorted, List<int> sorted) {
       shellSortMutating(testList0);
       expect(testList0, equals(sorted));
     });
-    print("Ending shellsort testing!\n");
   });
 }
 
 void testBubbleSortGroup(List<int> unsorted, List<int> sorted) {
   group('Testing Bubble Sort variations.', () {
     test('Test bubbleSort', () {
-      print("Testing bubblesort");
-      print("Unsorted list: $unsorted");
       expect(bubbleSort(unsorted), equals(sorted));
-      print("Unsorted list: $unsorted");
     });
     test('Test bubbleSortMutating', () {
-      print("Testing in-place, or mutating bubblesort.");
       expect(unsorted, isNot(sorted),
           reason:
               "To using sorted lists as inputs, and avoid false successes.");
